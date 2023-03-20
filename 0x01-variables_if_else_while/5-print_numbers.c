fc92b596213c18442fea022615944f6ef3bb4407
Emmanuel <blessedemma942@hotmail.com>
2023-03-20 06:41:09 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 successful
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 0; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
